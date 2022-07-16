96f63c7b8ded168686305648c480baca04de5838
Marnin Audu <msmaudu@gmail.com>
2022-07-16 16:44:50 +0100

    Removed backgrounded image. Added bodyparser, Express-session, and UUID. Created router file

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 // Import Modules
 const express = require("express");
+const bodyparser = require("body-parser")
 const app = express();
 const path = require("path");
+const session = require("express-session")
+const {v4: uuidv4} =require("uuid")
 
 
 // Port value
 const port = process.env.PORT || 4000;
+
+//Parse file body
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded(({extended:true})))
+
+//Create site session
+app.use(session({
+  secret: uuidv4(),
+  resave: false,
+  saveUninitialized: true
+}))
 
 //Load static assets
 app.use("/static", express.static(path.join(__dirname, 'public')))
@@ -17,7 +31,7 @@ app.set("view engine", "ejs");
 // Server
 app.get("/", (req, res) => {
 //Get method
-  res.render("index", { title: "Login page" });
+  res.render("index", { title: "Group B" });
 });
 
 // Post method
