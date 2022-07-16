0bbc72c890cdd5fc38062e775dbc798943c10aef
Marnin Audu <msmaudu@gmail.com>
2022-07-16 13:32:15 +0100

    Added bootstrap assets

--- server.js
+++ server.js
@@ -1,19 +1,22 @@
 // Handles requests and response
 const express = require("express");
+
 // File handling
 const app = express();
+
 // Port value
 const port = process.env.PORT || 4000;
+
 //Enables server HTML file rendering
 app.set("view engine", "ejs");
 
 // Server
 app.get("/", (req, res) => {
-  //Get method
+//Get method
   res.render("index", { title: "Login page" });
 });
 
-// app.post();
+// Post method
 app.listen(port, () =>
   console.log("Listening to the server on http://localhost:4000")
 );
