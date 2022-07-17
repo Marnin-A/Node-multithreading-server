e58fd178596ab270fcddca2aefa14e7aacb0151d
Marnin Audu <msmaudu@gmail.com>
2022-07-17 19:10:05 +0100

    Updated the dashboard to use username

--- router.js
+++ router.js
@@ -2,19 +2,28 @@ var express = require("express")
 var router = express.Router()
 
 const credential = {
-    email:"marnin@gmail.com",
+    Username:"Marnin",
     password:"1234"
 } 
 
 //User login
 router.post("/login", (req, res)=>{
-    if (req.body.email == credential.email && req.body.password==credential.password) {
-        req.session.user=req.body.email;
-        res.redirect("/dashboard")
+    if (req.body.Username == credential.Username && req.body.password==credential.password) {
+        req.session.user=req.body.Username;
+        res.redirect("/route/dashboard")
         // res.end("Login Successful")
     }else{
         res.end("Invalid Username or Password")
     }
+
+    //Dashboard Routing
+    router.get("/dashboard", (req,res)=>{
+        if (req.session.user) {
+            res.render("dashboard",{user:req.session.user})
+        } else {
+            res.send("Unauthorized user")
+        }
+    })
 })
 
 module.exports = router;
